cff080e61adc297e3fad7fd2beefe69035a28774
Mike Deskin <michaelhaydendeskin@gmail.com>
2015-11-14 05:14:05 +0000

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,20 @@
-# Implement your procedural solution here!
+def prime_number_for(nth_element)
+  prime_count = 0
+  prime_target = 2
+  prime_factor = 2
+
+until prime_count == nth_element
+  while prime_factor <= prime_target / 2
+      if prime_target % prime_factor == 0
+        prime_target += 1
+        prime_factor = 2
+      else
+        prime_factor += 1
+      end
+    end
+  prime_count += 1
+  prime_target += 1
+  prime_factor = 2
+  end
+prime_target -= 1
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,33 @@
-# Implement your object-oriented solution here!
+class Prime
+
+  def initialize(input)
+     @input = input
+   end
+  def input=(input)
+    @limit = input
+  end
+  def input
+    @input
+  end
+  
+  def number
+    prime_count = 0
+    prime_target = 2
+    prime_factor = 2
+
+  until prime_count == @input
+    while prime_factor <= prime_target / 2
+        if prime_target % prime_factor == 0
+          prime_target += 1
+          prime_factor = 2
+        else
+          prime_factor += 1
+        end
+      end
+    prime_count += 1
+    prime_target += 1
+    prime_factor = 2
+    end
+  prime_target -= 1
+  end
+end
